e2bcd3decdd5b8a3063c8976a1c6ebdd980575b2
leeminjung1 <vipminjung@gmail.com>
2020-09-26 22:06:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/doubleslash/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.doubleslash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new SplashHandler(), 3000);
+    }
+
+        private class SplashHandler implements Runnable{
+            public void run(){
+                startActivity(new Intent(getApplication(), MainActivity.class)); //로딩이 끝난 후, ChoiceFunction 이동
+                SplashActivity.this.finish(); // 로딩페이지 Activity stack에서 제거
+            }
+        }
+
+}
