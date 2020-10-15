336b37391dff053fa6664eb7af825411b96a227d
leeminjung1 <vipminjung@gmail.com>
2020-10-15 19:44:19 +0900

    login/signup completed

--- app/src/main/java/com/example/doubleslash/CustomDialog.java
+++ app/src/main/java/com/example/doubleslash/CustomDialog.java
@@ -1,32 +1,66 @@
 package com.example.doubleslash;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 
 import androidx.annotation.RequiresApi;
 
-import java.util.Objects;
-
 public class CustomDialog extends Dialog {
 
     private ImageButton deleteBtn;
     private View.OnClickListener cancelListener;
+    private int width;
+    private int height;
+
+    public CustomDialog(Context context, View.OnClickListener cancelListener) {
+        super(context);
+        this.cancelListener = cancelListener;
+        setContentView(R.layout.custom_dialog);
+        deleteBtn= (ImageButton)findViewById(R.id.deleteBtn);
+        deleteBtn.setOnClickListener(cancelListener);
+    }
+
+    public void createDialog(CustomDialog dialog)
+    {
+        width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 260, getContext().getResources().getDisplayMetrics());
+        height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,100, getContext().getResources().getDisplayMetrics());
+
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        Window window = dialog.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.setDimAmount(0.4f);
+
+        layoutParams.copyFrom(window.getAttributes());
+        layoutParams.width = width;
+        layoutParams.height = height;
+        window.setAttributes(layoutParams);
+
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCancelable(true);
+        dialog.show();
+    }
 
 
+
+    /*
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_dialog);
 
+        //클릭 리스너 셋팅 (클릭버튼이 동작하도록 만들어줌.)
         deleteBtn= (ImageButton)findViewById(R.id.deleteBtn);
+        deleteBtn.setOnClickListener(cancelListener);
+
 
         //다이얼로그 밖의 화면은 흐리게 만들어줌
         WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
@@ -34,26 +68,19 @@ public class CustomDialog extends Dialog {
         layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         layoutParams.dimAmount = 0.4f;
 
-        //dp로 변경하기
-        //Objects.requireNonNull(getWindow()).setAttributes(layoutParams);
+        width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 254, getContext().getResources().getDisplayMetrics());
+        height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,85, getContext().getResources().getDisplayMetrics());
 
-        final int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,300, getContext().getResources().getDisplayMetrics());
-        final int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,100, getContext().getResources().getDisplayMetrics());
         layoutParams.width = width;
         layoutParams.height = height;
 
+   }
 
+     */
 
-        //클릭 리스너 셋팅 (클릭버튼이 동작하도록 만들어줌.)
-        deleteBtn.setOnClickListener(cancelListener);
-
-    }
 
 
     //생성자 생성
-    public CustomDialog(Context context, View.OnClickListener cancelListener) {
-        super(context);
-        this.cancelListener = cancelListener;
-    }
+
 
 }
